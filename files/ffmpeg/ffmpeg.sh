080e34136faa85b00688c5b954989c1584ff0648
Gerry Demaret <gerry@x-net.be>
2017-03-14 23:23:28 +0100

    Add proper timestamps to the dump files

--- files/ffmpeg/ffmpeg.sh
+++ files/ffmpeg/ffmpeg.sh
@@ -3,6 +3,7 @@
 . /opt/ffmpeg-config.sh
 
 ffmpeg -i "${SOURCE}" \
-    -c copy -flags +global_header -f segment -segment_time ${RECORDING_SEGMENT_LENGTH} \
-    	-segment_format_options movflags=+faststart -reset_timestamps 1 -bsf:a aac_adtstoasc "${RECORDING_DIRECTORY}out%d.mp4" \
+    -c copy -flags +global_header \
+        -f segment -segment_time ${RECORDING_SEGMENT_LENGTH} -segment_format_options movflags=+faststart -strftime 1 \
+        -reset_timestamps 1 -bsf:a aac_adtstoasc "${RECORDING_DIRECTORY}channel-%Y%m%d-%H%M%S.mp4" \
     -c copy -c:a aac -f flv "${DESTINATION}"
